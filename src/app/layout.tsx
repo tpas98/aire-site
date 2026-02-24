678159da6e54539464f736a2cc993c8e5da07d58
Thomas Pasyanos <thomaspasyanos@Thomass-MacBook-Air.local>
2026-02-24 07:55:14 -0700

    Reduce hero padding and fix scroll restoration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" scroll-behavior="auto">
       <body>{children}</body>
     </html>
   )
