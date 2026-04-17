dc03676c8e541f8def7aabee24661d3693fe9100
Aire <ajiafrat@umich.edu>
2026-04-17 09:13:10 -0400

    Stop tracking local env and Next build output

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jYRNffo0vKXMXa2bhI7Eq+R4YinF0yvN98VA/uUiuto=\"}"
--- .next/static/chunks/app/privacy/page-b06967426a882662.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[385,571,799,553],{3137:function(e,n,s){Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[173,231,971,23,744],function(){return e(e.s=3137)}),_N_E=e.O()}]);
